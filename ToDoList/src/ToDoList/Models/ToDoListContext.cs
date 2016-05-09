6ef411699bed7707e4a9d954bf4205cdd15e98f8
jill kuchman <pillanoid@gmail.com>
2016-05-09 14:41:33 -0700

    Add classes and database info for migrations

---
+++ ToDoList/src/ToDoList/Models/ToDoListContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class ToDoListContext : DbContext
+    {
+        public virtual DbSet<Item> Items { get; set; }
+    }
+}
